2454e376640fd90e3de4d86d14b0ab8b4d0151f4
Veljic <veljic.ra31.2019@uns.ac.rs>
2023-10-31 17:46:28 +0100

    fix navbar

--- FrontEnd/GitHubSimulatorUI/src/app/pages/access-control/access-control.module.ts
+++ FrontEnd/GitHubSimulatorUI/src/app/pages/access-control/access-control.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { AccessControlComponent } from './access-control.component';
 import { AccessControlRoutingModule } from './access-control-routing.module';
 
+
 @NgModule({
   imports: [
     CommonModule,
--- FrontEnd/GitHubSimulatorUI/src/app/shared/navbar/navbar.module.ts
+++ FrontEnd/GitHubSimulatorUI/src/app/shared/navbar/navbar.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   imports: [
     CommonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule
   ],
   declarations: [NavbarComponent],
   exports: [NavbarComponent]
