7168c1d3965a3ef5df0921b28c9293013f33d20b
Mihailo Veljic <93385666+mihailoveljic@users.noreply.github.com>
2023-11-03 16:29:31 +0100

    2023.11.03_Test1 (#12)
    * [GS-2]: create front end build pipeline (#3)
    
    * pipeline_frontend_ci file created
    
    * Test CI pipeline
    
    * [GS-2]: create front end ci pipeline (#8)
    
    * pipeline_frontend_ci file created
    
    * Test CI pipeline
    
    * update filename
    
    * Update pipeline_frontend_ci.yml
    
    * Delete pipeline_frontend_ci
    
    * Update pipeline_frontend_ci.yml
    
    * Update pipeline_frontend_ci.yml
    
    * Update pipeline_frontend_ci.yml
    
    * Update pipeline_frontend_ci.yml
    
    * Update pipeline_frontend_ci.yml
    
    * Update pipeline_frontend_ci.yml
    
    * Update pipeline_frontend_ci.yml
    
    * Pipeline fix
    
    * next try
    
    * Update pipeline_frontend_ci.yml
    
    * Update navbar.component.html
    
    * Update pipeline_frontend_ci.yml
    
    * Update navbar.component.html
    
    * Feature/GS-2 create front end ci pipeline (#10)
    
    * pipeline_frontend_ci file created
    
    * Test CI pipeline
    
    * update filename
    
    * Update pipeline_frontend_ci.yml
    
    * Delete pipeline_frontend_ci
    
    * Update pipeline_frontend_ci.yml
    
    * Update pipeline_frontend_ci.yml
    
    * Update pipeline_frontend_ci.yml
    
    * Update pipeline_frontend_ci.yml
    
    * Update pipeline_frontend_ci.yml
    
    * Update pipeline_frontend_ci.yml
    
    * Update pipeline_frontend_ci.yml
    
    * Pipeline fix
    
    * next try
    
    * Update pipeline_frontend_ci.yml
    
    * Update navbar.component.html
    
    * Update pipeline_frontend_ci.yml
    
    * Update navbar.component.html
    
    * pipeline to push docker image to docker hub (#11)

--- BackEnd/GitHubSimulator/Controllers/WeatherForecastController.cs
+++ BackEnd/GitHubSimulator/Controllers/WeatherForecastController.cs
@@ -28,4 +28,16 @@ public class WeatherForecastController : ControllerBase
         })
         .ToArray();
     }
+
+    [HttpGet("test",Name = "GetWeatherForecastTest")]
+    public IEnumerable<WeatherForecast> GetTest()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
 }
