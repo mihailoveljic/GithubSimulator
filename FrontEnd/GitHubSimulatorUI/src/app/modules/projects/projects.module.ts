552d718ec926001845e7ef93fd11eb67ef19fbeb
Radisa <stojkic.ra84.2019@uns.ac.rs>
2023-11-27 20:04:14 +0100

    Pages have been added into the module

--- FrontEnd/GitHubSimulatorUI/src/app/modules/projects/projects.module.ts
+++ FrontEnd/GitHubSimulatorUI/src/app/modules/projects/projects.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PageComponent } from './page/page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PageComponent
+  ],
   imports: [
     CommonModule
   ]
--- FrontEnd/GitHubSimulatorUI/src/app/shared/navbar/navbar.module.ts
+++ FrontEnd/GitHubSimulatorUI/src/app/shared/navbar/navbar.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+
 
 @NgModule({
   imports: [
     CommonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule,
+    MatIconModule,
+    MatMenuModule,
+    MatIconModule
   ],
   declarations: [NavbarComponent],
   exports: [NavbarComponent]
