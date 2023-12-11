bce8c524fb0ba0741f94381278024a1846611227
Nemanja Andrić <nandric00@gmail.com>
2023-12-11 20:28:02 +0100

    Add Authorize annotation for all new controllers

--- BackEnd/GitHubSimulator/Controllers/CommentController.cs
+++ BackEnd/GitHubSimulator/Controllers/CommentController.cs
@@ -2,102 +2,104 @@
 using CSharpFunctionalExtensions;
 using GitHubSimulator.Core.BuildingBlocks.Exceptions;
 using GitHubSimulator.Core.Interfaces.Services;
-using Microsoft.AspNetCore.Mvc;
 using GitHubSimulator.Core.Models.Entities;
 using GitHubSimulator.Dtos.Comments;
 using GitHubSimulator.Factories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GitHubSimulator.Controllers;
 
 [ApiController]
+[Authorize]
 [Route("[controller]")]
 public class CommentController : ControllerBase
 {
-    public CommentController(ICommentService commentService, CommentFactory commentFactory)
-    {
-        _commentService = commentService;
-        _commentFactory = commentFactory;
-    }
+	public CommentController(ICommentService commentService, CommentFactory commentFactory)
+	{
+		_commentService = commentService;
+		_commentFactory = commentFactory;
+	}
 
-    private readonly ICommentService _commentService;
-    private readonly CommentFactory _commentFactory;
+	private readonly ICommentService _commentService;
+	private readonly CommentFactory _commentFactory;
 
-    [HttpGet(Name = "GetAllComments")]
-    public async Task<IActionResult> GetAllComments()
-    {
-        try
-        {
-            return Ok(await _commentService.GetAll());
-        }
-        catch (Exception ex)
-        {
-            return BadRequest(ex.Message);
-        }
-    }
+	[HttpGet(Name = "GetAllComments")]
+	public async Task<IActionResult> GetAllComments()
+	{
+		try
+		{
+			return Ok(await _commentService.GetAll());
+		}
+		catch (Exception ex)
+		{
+			return BadRequest(ex.Message);
+		}
+	}
 
-    [HttpGet("{id:guid}", Name = "GetCommentById")]
-    [SuppressMessage("ReSharper", "ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract")]
-    public async Task<IActionResult> GetCommentById(Guid id)
-    {
-        var response = await _commentService.GetById(id);
-        if (response is null)
-        {
-            return NotFound("A comment with the provided ID not found");
-        }
+	[HttpGet("{id:guid}", Name = "GetCommentById")]
+	[SuppressMessage("ReSharper", "ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract")]
+	public async Task<IActionResult> GetCommentById(Guid id)
+	{
+		var response = await _commentService.GetById(id);
+		if (response is null)
+		{
+			return NotFound("A comment with the provided ID not found");
+		}
 
-        return Ok(response);
-    }
+		return Ok(response);
+	}
 
-    [HttpPost]
-    public async Task<IActionResult> CreateComment([FromBody] InsertCommentDto dto)
-    {
-        try
-        {
-            var result = await _commentService.Insert(_commentFactory.MapToDomain(dto));
-            return Created("Comment successfully created", result);
-        }
-        catch (InvalidTaskForCommentException te)
-        {
-            return BadRequest(te.Message);
-        }
-        catch (FluentValidation.ValidationException ve)
-        {
-            return BadRequest("Fluent validation error: " + ve.Message);
-        }
-        catch (Exception e)
-        {
-            return StatusCode(500, "Internal Server Error: " + e.Message);
-        }
-    }
+	[HttpPost]
+	public async Task<IActionResult> CreateComment([FromBody] InsertCommentDto dto)
+	{
+		try
+		{
+			var result = await _commentService.Insert(_commentFactory.MapToDomain(dto));
+			return Created("Comment successfully created", result);
+		}
+		catch (InvalidTaskForCommentException te)
+		{
+			return BadRequest(te.Message);
+		}
+		catch (FluentValidation.ValidationException ve)
+		{
+			return BadRequest("Fluent validation error: " + ve.Message);
+		}
+		catch (Exception e)
+		{
+			return StatusCode(500, "Internal Server Error: " + e.Message);
+		}
+	}
 
-    [HttpPut]
-    public async Task<IActionResult> UpdateComment([FromBody] UpdateCommentDto dto)
-    {
-        try
-        {
-            var response = await _commentService.Update(_commentFactory.MapToDomain(dto));
-            if (response.Equals(Maybe<Comment>.None))
-            {
-                return NotFound("A comment with the provided ID not found");
-            }
+	[HttpPut]
+	public async Task<IActionResult> UpdateComment([FromBody] UpdateCommentDto dto)
+	{
+		try
+		{
+			var response = await _commentService.Update(_commentFactory.MapToDomain(dto));
+			if (response.Equals(Maybe<Comment>.None))
+			{
+				return NotFound("A comment with the provided ID not found");
+			}
 
-            return Ok(response.Value);
-        }
-        catch (FluentValidation.ValidationException ve)
-        {
-            return BadRequest(ve.Message);
-        }
-    }
+			return Ok(response.Value);
+		}
+		catch (FluentValidation.ValidationException ve)
+		{
+			return BadRequest(ve.Message);
+		}
+	}
 
-    [HttpDelete("{id:guid}")]
-    public async Task<IActionResult> DeleteComment(Guid id)
-    {
-        var response = await _commentService.Delete(id);
-        if (response)
-        {
-            return Ok("Comment deleted successfully");
-        }
+	[HttpDelete("{id:guid}")]
+	public async Task<IActionResult> DeleteComment(Guid id)
+	{
+		var response = await _commentService.Delete(id);
+		if (response)
+		{
+			return Ok("Comment deleted successfully");
+		}
 
-        return NotFound("A comment with provided ID not found");
-    }
+		return NotFound("A comment with provided ID not found");
+	}
 }
--- BackEnd/GitHubSimulator/Controllers/PullRequestController.cs
+++ BackEnd/GitHubSimulator/Controllers/PullRequestController.cs
@@ -3,72 +3,76 @@ using GitHubSimulator.Core.Interfaces.Services;
 using GitHubSimulator.Core.Models.Entities;
 using GitHubSimulator.Dtos.PullRequests;
 using GitHubSimulator.Factories;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GitHubSimulator.Controllers;
 
 [ApiController]
+[Authorize]
 [Route("[controller]")]
 public class PullRequestController : ControllerBase
 {
-    private readonly IPullRequestService pullRequestService;
-    private readonly ILogger<PullRequestController> logger;
-    private readonly PullRequestFactory pullRequestFactory;
+	private readonly IPullRequestService pullRequestService;
+	private readonly ILogger<PullRequestController> logger;
+	private readonly PullRequestFactory pullRequestFactory;
 
-    public PullRequestController(
-        IPullRequestService pullRequestService,
-        ILogger<PullRequestController> logger,
-        PullRequestFactory pullRequestFactory)
-    {
-        this.pullRequestService = pullRequestService;
-        this.logger = logger;
-        this.pullRequestFactory = pullRequestFactory;
-    }
+	public PullRequestController(
+		IPullRequestService pullRequestService,
+		ILogger<PullRequestController> logger,
+		PullRequestFactory pullRequestFactory)
+	{
+		this.pullRequestService = pullRequestService;
+		this.logger = logger;
+		this.pullRequestFactory = pullRequestFactory;
+	}
 
-    [HttpGet("All", Name = "GetAllPullRequests")]
-    public async Task<IActionResult> GetAllPullRequestss()
-    {
-        try
-        {
-            return Ok(await pullRequestService.GetAll());
-        }
-        catch (Exception ex)
-        {
-            return BadRequest(ex.Message);
-        }
-    }
+	[HttpGet("All", Name = "GetAllPullRequests")]
+	public async Task<IActionResult> GetAllPullRequestss()
+	{
+		try
+		{
+			return Ok(await pullRequestService.GetAll());
+		}
+		catch (Exception ex)
+		{
+			return BadRequest(ex.Message);
+		}
+	}
 
-    [HttpGet(Name = "GetPullRequestById")]
-    public async Task<IActionResult> GetById([FromQuery] Guid id)
-    {
-        return (await pullRequestService.GetById(id))
-        .Map(pullRequest => (IActionResult)Ok(pullRequest))
-        .GetValueOrDefault(() => {
-            return NotFound();
-        });
-    }
+	[HttpGet(Name = "GetPullRequestById")]
+	public async Task<IActionResult> GetById([FromQuery] Guid id)
+	{
+		return (await pullRequestService.GetById(id))
+		.Map(pullRequest => (IActionResult)Ok(pullRequest))
+		.GetValueOrDefault(() =>
+		{
+			return NotFound();
+		});
+	}
 
-    [HttpPost]
-    public async Task<ActionResult<PullRequest>> CreatePullRequest([FromBody] InsertPullRequestDto dto)
-    {
-        return Created(
-            "https://www.youtube.com/watch?v=LTyZKvIxrDg&t=3566s&ab_channel=Standuprs", 
-            await pullRequestService.Insert(pullRequestFactory.MapToDomain(dto)));
-    }
+	[HttpPost]
+	public async Task<ActionResult<PullRequest>> CreatePullRequest([FromBody] InsertPullRequestDto dto)
+	{
+		return Created(
+			"https://www.youtube.com/watch?v=LTyZKvIxrDg&t=3566s&ab_channel=Standuprs",
+			await pullRequestService.Insert(pullRequestFactory.MapToDomain(dto)));
+	}
 
-    [HttpPut]
-    public async Task<IActionResult> UpdatePullRequest([FromBody] UpdatePullRequestDto dto)
-    {
-        return (await pullRequestService.Update(pullRequestFactory.MapToDomain(dto)))
-        .Map(pullRequest => (IActionResult)Ok(pullRequest))
-        .GetValueOrDefault(() => {
-            return NotFound();
-        });
-    }
+	[HttpPut]
+	public async Task<IActionResult> UpdatePullRequest([FromBody] UpdatePullRequestDto dto)
+	{
+		return (await pullRequestService.Update(pullRequestFactory.MapToDomain(dto)))
+		.Map(pullRequest => (IActionResult)Ok(pullRequest))
+		.GetValueOrDefault(() =>
+		{
+			return NotFound();
+		});
+	}
 
-    [HttpDelete]
-    public async Task<ActionResult<bool>> DeletePullRequest([FromQuery] Guid id)
-    {
-        return Ok(await pullRequestService.Delete(id));
-    }
+	[HttpDelete]
+	public async Task<ActionResult<bool>> DeletePullRequest([FromQuery] Guid id)
+	{
+		return Ok(await pullRequestService.Delete(id));
+	}
 }
