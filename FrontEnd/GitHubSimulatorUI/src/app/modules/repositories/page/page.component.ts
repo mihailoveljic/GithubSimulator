816e41c51292a548c58320afb3520cf15c0f75a8
Nemanja Andrić <nandric00@gmail.com>
2024-02-13 19:04:02 +0100

    Create repository component

--- FrontEnd/GitHubSimulatorUI/src/app/modules/repositories/page/page.component.ts
+++ FrontEnd/GitHubSimulatorUI/src/app/modules/repositories/page/page.component.ts
@@ -6,6 +6,7 @@ import { Visibility } from '../model/Visibility';
 import { MatDialog } from '@angular/material/dialog';
 import { RepositoryDetailsDialogComponent } from '../dialog/repository_details_dialog/repository_details_dialog.component';
 import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page',
@@ -20,60 +21,64 @@ export class PageComponent {
   constructor(
     private repositoryService: RepositoryService,
     public dialog: MatDialog,
-    public toastr: ToastrService) {}
+    public toastr: ToastrService,
+    private router: Router) {}
 
   ngOnInit() {
     this.repositories = this.repositoryService.getAllRepositories();
   }
 
   openDialog(repository: Repository | undefined) {
-    if(repository){
-      const dialogRef = this.dialog.open(RepositoryDetailsDialogComponent, {
-        data: repository
-      });
-      dialogRef.afterClosed().subscribe((result) => {
-        console.log(result);
-        if(result.name){
-            this.repositoryService.updateRepository(result).subscribe({
-              next: (repo) => {
-                this.repositories = this.repositoryService.getAllRepositories();
-                this.toastr.success('${name} Successfully updated', repo.name);
-              },
-              error: (e) => {
-                this.toastr.error(e.error);
-              }
-            });
-          }
-          else if(result) {
-            this.repositoryService.deleteRepository(result).subscribe({
-              next: (repo) => {
-                this.repositories = this.repositoryService.getAllRepositories();
-                this.toastr.success(repository.name + ' Successfully deleted');
-              },
-              error: (e) => {
-                console.log(e);
-                this.toastr.error(e.error);
-              }
-            });
-          }
-        });
-    }
-    else {
-      const dialogRef = this.dialog.open(RepositoryDetailsDialogComponent);
-      dialogRef.afterClosed().subscribe(result => {
-        if(result){
-            this.repositoryService.createRepository(result).subscribe({
-              next: (repo) => {
-                this.repositories = this.repositoryService.getAllRepositories();
-                this.toastr.success(repo.name + 'Successfully created');
-              },
-              error: (e) => {
-                this.toastr.error(e.error);
-              }
-            });
-          }
-        });
-    }
+
+    this.router.navigate(['/new-repository']);
+
+    // if(repository){
+    //   const dialogRef = this.dialog.open(RepositoryDetailsDialogComponent, {
+    //     data: repository
+    //   });
+    //   dialogRef.afterClosed().subscribe((result) => {
+    //     console.log(result);
+    //     if(result.name){
+    //         this.repositoryService.updateRepository(result).subscribe({
+    //           next: (repo) => {
+    //             this.repositories = this.repositoryService.getAllRepositories();
+    //             this.toastr.success('${name} Successfully updated', repo.name);
+    //           },
+    //           error: (e) => {
+    //             this.toastr.error(e.error);
+    //           }
+    //         });
+    //       }
+    //       else if(result) {
+    //         this.repositoryService.deleteRepository(result).subscribe({
+    //           next: (repo) => {
+    //             this.repositories = this.repositoryService.getAllRepositories();
+    //             this.toastr.success(repository.name + ' Successfully deleted');
+    //           },
+    //           error: (e) => {
+    //             console.log(e);
+    //             this.toastr.error(e.error);
+    //           }
+    //         });
+    //       }
+    //     });
+    // }
+    // else {
+    //   const dialogRef = this.dialog.open(RepositoryDetailsDialogComponent);
+    //   dialogRef.afterClosed().subscribe(result => {
+    //     if(result){
+    //         this.repositoryService.createRepository(result).subscribe({
+    //           next: (repo) => {
+    //             this.repositories = this.repositoryService.getAllRepositories();
+    //             this.toastr.success(repo.name + 'Successfully created');
+    //           },
+    //           error: (e) => {
+    //             this.toastr.error(e.error);
+    //           }
+    //         });
+    //       }
+    //     });
+    // }
 
   }
 }
